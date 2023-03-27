b1261638e41947d43e22399a008e26d38252cfce
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-03-27 17:53:32 +0100

    home route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const blogRoutes = require('./blogRoutes');
-const commentRoutes = require('./commentRoutes');
-const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blog-routes');
+const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/blogs', blogRoutes);
 router.use('/comments', commentRoutes);
